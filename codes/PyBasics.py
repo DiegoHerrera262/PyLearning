aa0d55f248d4a626b96efa34c048aadca2e19704
DiegoHerrera262 <diegoherrera262@gmail.com>
2019-12-23 13:35:53 -0500

    First Basic Routine with Python

--- codes/PyBasics.py
+++ codes/PyBasics.py
@@ -1,16 +1,38 @@
 # Program to show Python basics
 
-def is_even(n):
-    if n % 2 == 0:
-        print(n, "is even")
-        return(1)
+# Date: 23-12-19
+# AUthor: Diego Herrera
+# Description: This program implements a recursive bisection algorithm to find
+#              square root of real positive number.
+
+import math as mt
+
+def bisection(num, guess_inf, guess_sup):
+    fi = guess_inf**2 - num
+    fs = guess_sup**2 - num
+    # Definition of root by bisection
+    root = 0.5*(guess_inf + guess_sup)
+    fr = root**2 - num
+    # Condition to finish (inner stack)
+    if mt.fabs(fr) < 1e-5:
+        print("I found: ", root)
+    # Recursive call
     else:
-        print(n, "is odd")
-        return(0)
+        print("Still in: ", root)
+        if fi*fr > 0:
+            bisection(num, root, guess_sup)
+        elif fs*fr > 0:
+            bisection(num, guess_inf, root)
+        else:
+            print("bad luck")
 
-num = input("Enter an integer: ")         # Input captures a string
-num = int(num)                           # Convert to integer type
+# Main program execution
+    
+num = input("I'll find square root of: ")         # Input captures a string
+num = float(num)                                  # Convert to integer type
+seed_inf = input("with inf. seed: ")              # Input captures a string
+seed_inf = float(seed_inf)
+seed_sup = input("with sup. seed: ")              # Input captures a string
+seed_sup = float(seed_sup)
 
-val = is_even(num)
-print(val)
-
+bisection(num,seed_inf,seed_sup)                  # Uses sure seeds
